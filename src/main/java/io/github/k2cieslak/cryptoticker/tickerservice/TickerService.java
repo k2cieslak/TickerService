d328f7784eb2b8f4b2954d2e5619a715efe7dd56
Krzysztof Cieślak <kcieslak@outlook.com>
2018-09-20 10:28:13 +0200

    Project setup & initial commit

---
+++ src/main/java/io/github/k2cieslak/cryptoticker/tickerservice/TickerService.java
@@ -0,0 +1,14 @@
+package io.github.k2cieslak.cryptoticker.tickerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TickerService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickerService.class, args);
+    }
+
+}
