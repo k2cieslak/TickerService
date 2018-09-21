0881dbafbfc27101aa962f17dfa6df8d96568df6
Krzysztof Cieślak <kcieslak@outlook.com>
2018-09-21 09:26:37 +0200

    added missing resources

---
+++ src/main/java/io/github/k2cieslak/cryptoticker/tickerservice/configuration/AppConfiguration.java
@@ -0,0 +1,18 @@
+package io.github.k2cieslak.cryptoticker.tickerservice.configuration;
+
+import io.github.k2cieslak.cryptoticker.tickerservice.marketdata.MarketDataBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = " io.github.k2cieslak.cryptoticker.tickerservice.marketdata")
+public class AppConfiguration {
+
+    @Bean
+    public MarketDataBean marketData() {
+        return new MarketDataBean();
+    }
+
+
+}
---
+++ src/main/java/io/github/k2cieslak/cryptoticker/tickerservice/exception/TickerServiceException.java
@@ -0,0 +1,4 @@
+package io.github.k2cieslak.cryptoticker.tickerservice.exception;
+
+public class TickerServiceException extends Exception {
+}
